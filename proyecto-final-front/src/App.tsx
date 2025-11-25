8abf393d1166098dcac34e4d4af26ff8c6e07c1e
Mateo Muscolino <muscolinomateo@gmail.com>
2025-11-25 10:56:20 -0300

    Configuracion de ruteo (React Router)

--- proyecto-final-front/src/App.tsx
+++ proyecto-final-front/src/App.tsx
@@ -1,19 +1,22 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import DashboardPage from './pages/DashboardPage';
+
 function App() {
   return (
-    <div className="flex h-screen w-full items-center justify-center bg-gray-900">
-      <div className="text-center">
-        <h1 className="text-5xl font-bold text-blue-500 mb-4">
-          Prueba de Tailwind CSS
-        </h1>
-        <p className="text-xl text-gray-300">
-          React + TypeScript + Tailwind CSS configurado correctamente.
-        </p>
-        <button className="mt-6 px-6 py-2 bg-blue-600 hover:bg-blue-700 text-white rounded-lg transition-colors">
-          Botón de Prueba
-        </button>
-      </div>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        {/* ruta publica para logearse */}
+        <Route path="/login" element={<LoginPage />} />
+        
+        {/* esta ruta tendria que ir privada */}
+        <Route path="/dashboard" element={<DashboardPage />} />
+
+        {/* link default te redirecciona a login */}
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ proyecto-final-front/src/pages/DashboardPage.tsx
@@ -0,0 +1,10 @@
+const DashboardPage = () => {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold text-green-600">Dashboard Principal</h1>
+      <p> gráficos y reportes.</p>
+    </div>
+  );
+};
+
+export default DashboardPage;
