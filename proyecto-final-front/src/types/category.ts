db7ba21489a10aa9274b0c8acd79d394695a268a
Mateo Muscolino <muscolinomateo@gmail.com>
2025-11-27 20:02:13 -0300

    Comentarios agregados type category

--- proyecto-final-front/src/types/category.ts
+++ proyecto-final-front/src/types/category.ts
@@ -1,10 +1,10 @@
-export interface Category {
+export interface Category { // datos que vienen de la base de datos
   _id: string;
   name: string;
   description: string;
 }
 
-export interface CategoryPayload {
+export interface CategoryPayload { // datos que el usuario ingresa al formulario, no tiene id porque mongo lo crea despues
   name: string;
   description: string;
 }
