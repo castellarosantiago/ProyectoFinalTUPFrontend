79c3148074596a52a732ec46a428fc07769a491d
Mateo Muscolino <muscolinomateo@gmail.com>
2025-11-27 20:02:30 -0300

    Comentarios agregados type product

--- proyecto-final-front/src/types/product.ts
+++ proyecto-final-front/src/types/product.ts
@@ -1,4 +1,4 @@
-export interface Product {
+export interface Product { // datos que vienen de la base de datos
   _id: string;
   id_category: string;
   name: string;
@@ -6,8 +6,8 @@ export interface Product {
   stock: number;
 }
 
-// creamos o editamos sin el _id
-export interface ProductPayload {
+// datos que el usuario ingresa al formulario, no tiene id porque mongo lo crea despues
+export interface ProductPayload { 
   id_category: string;
   name: string;
   price: number;
