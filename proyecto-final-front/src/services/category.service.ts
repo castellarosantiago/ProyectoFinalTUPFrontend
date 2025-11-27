3f05cac61d64e18c1b9cdae0a8224a8c50aa12a3
Mateo Muscolino <muscolinomateo@gmail.com>
2025-11-27 20:01:43 -0300

    Comentarios agregados category.service

--- proyecto-final-front/src/services/category.service.ts
+++ proyecto-final-front/src/services/category.service.ts
@@ -7,9 +7,9 @@ export const CategoryService = {
     const res = await fetch(API_URL);
     if (!res.ok) throw new Error("Error al obtener categorías");
     return res.json();
-  },
+  }, // retorna una promesa con el array de categorías (Category[])
 
-  create: async (payload: CategoryPayload): Promise<Category> => {
+  create: async (payload: CategoryPayload): Promise<Category> => { // recibe los datos limpios sin el id
     const res = await fetch(API_URL, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -18,9 +18,9 @@ export const CategoryService = {
     if (!res.ok) throw new Error("Error al crear categoría");
     const data = await res.json();
     return data.category; // devuelve { message, category }
-  },
+  }, // retorna la categoia creada con el id nuevo generado por mongo
 
-  update: async (id: string, payload: CategoryPayload): Promise<Category> => {
+  update: async (id: string, payload: CategoryPayload): Promise<Category> => { // recibe el id para saber cual actualizar y los datos nuevos sin el id
     const res = await fetch(`${API_URL}/${id}`, {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
@@ -29,10 +29,10 @@ export const CategoryService = {
     if (!res.ok) throw new Error("Error al actualizar categoría");
     const data = await res.json();
     return data.category; // devuelve { message, category }
-  },
+  }, // retorna la categoria actualizada
 
-  delete: async (id: string): Promise<void> => {
+  delete: async (id: string): Promise<void> => { //recibe el id de la categoría a eliminar
     const res = await fetch(`${API_URL}/${id}`, { method: "DELETE" });
     if (!res.ok) throw new Error("Error al eliminar categoría");
-  }
+  } // no retorna nada
 };
