8a70f0a82b672d3ea7d2c24e5120893f4cccae2a
Mateo Muscolino <muscolinomateo@gmail.com>
2025-11-28 09:57:01 -0300

    Nueva Page SaleRegister

--- proyecto-final-front/src/App.tsx
+++ proyecto-final-front/src/App.tsx
@@ -3,6 +3,7 @@ import LoginPage from './pages/LoginPage';
 import DashboardPage from './pages/DashboardPage';
 import CategoriesManager from './pages/CategoriesManager';
 import ProductsManager from './pages/ProductManager';
+import SalesRegister from './pages/SaleRegister';
 
 function App() {
   return (
@@ -10,10 +11,10 @@ function App() {
       <Routes>
         {/* ruta publica para logearse */}
         <Route path="/login" element={<LoginPage />} />
-        
         {/* esta ruta tendria que ir privada */}
         <Route path="/dashboard" element={<DashboardPage />} />
-
+        {/* ruta para registrar ventas */}
+        <Route path="/sales" element={<SalesRegister />} />
         {/* ruta para la gestión de categorias */}
         <Route path="/category" element={<CategoriesManager />} />
         {/* ruta para la gestión de prodcutos */}
